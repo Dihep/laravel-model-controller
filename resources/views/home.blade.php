a91108941dd8a29eabd357f12d670d4a82670d63
Dihep <dihep.demayo@gmail.com>
2024-07-10 23:28:32 +0200

    Passaggio dei dati alla vista home, con errore in stampa.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Dati da "config/data.php": {{ $home }}</h1>
-<p>This is my paragraph content.</p>
+<h1>Dati dal server:</h1>
+<div id="moviesContainer">
+    @foreach ($movies as $movie => $key)
+    <div class="card">
+        @foreach ($key as $element)
+            <p>{{$element}}</p>
+        @endforeach
+        {{-- @foreach ($key as $element => $detail)
+            <p>{{$loop->iteration}}</p>
+            <p>{{$detail}}</p>
+        @endforeach --}}
+    </div>
+    @endforeach
+</div>
+
 <button class="btn btn-primary">Cliccami</button>
 @endsection
